7e94a7f44b26146f718e601849074fe53ca24970
vitorfontenele <vitor_fontenele@hotmail.com>
2022-09-06 13:00:12 -0300

    Exercício 3 resolvido

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,9 +1,21 @@
-let idade;
-let isMaiorIdade;
-let isCursandoOutraFaculdade;
+let idade = 19;
+let terminouEnsinoMedio = true;
+let cursandoFaculdade = false;
 
-if(){
-
+if(idade >= 18){
+    console.log("É maior de idade");
 }else{
-	
+	console.log("É menor de idade");
+}
+
+if(terminouEnsinoMedio){
+    console.log("Terminou o Ensino Médio");
+} else {
+    console.log("Não terminou o Ensino Médio");
+}
+
+if(!cursandoFaculdade){
+    console.log("Não está na faculdade");
+} else {
+    console.log("Está na faculdade");
 }
