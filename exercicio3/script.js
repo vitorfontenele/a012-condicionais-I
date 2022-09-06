b05f63bb096473c20013d726286c8baa8bf42658
vitorfontenele <vitor_fontenele@hotmail.com>
2022-09-06 13:07:12 -0300

    Exercício 3 resolvido

--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,1 +1,10 @@
-let nacionalidade //= adicione o prompt aqui
+const nacionalidade = prompt("Escreva aqui sua nacionalidade:").toLowerCase();
+
+const nacionalidades = ["brasileira", "argentina", "uruguaia", "chilena", "colombiana"];
+
+if (nacionalidades.includes(nacionalidade)){
+    console.log(nacionalidade);
+} else {
+    console.log("nacionalidade não encontrada");
+}
+
